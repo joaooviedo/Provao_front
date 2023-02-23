be435ab6677004b64bb7ad4dfbfe18e63af41844
joaooviedo <joaovitorgiroldi@gmail.com>
2023-02-23 00:30:13 -0300

    Fix:

--- src/router.tsx
+++ src/router.tsx
@@ -2,14 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import { RoutePath } from "types/routes";
 import Home from "pages/Home/index";
 import Login from "pages/Login";
-import ManageUsers from "components/ManageUsers";
 
 const Router = () => {
   return (
     <Routes>
       <Route path={RoutePath.LOGIN} element={<Login />} />
       <Route path={RoutePath.HOME} element={<Home />} />
-      <Route path={RoutePath.USERS} element={<ManageUsers />} />
     </Routes>
   );
 };
